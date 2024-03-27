467928398d6e793ef6f7c1e172c315cabc34ee38
777uliahshafar <777uliahshafar@gmail.com>
2024-03-27 19:01:09 +0800

    miglink

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,41 +1,35 @@
-import React from 'react'
-import { Link, StaticQuery, graphql } from 'gatsby'
-import styles from './_nav.module.scss'
-
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Link from "next/link"
+import styles from "./_nav.module.scss"
 
 //NavLink component
 
 const NavLink = props => (
-    <Link className={styles.link} to={props.to}>{props.text}</Link>
+  <Link className={styles.link} href={props.to}>
+    <a>{props.text}</a>
+  </Link>
 )
 
-
-
 export default () => (
-
-    <StaticQuery
-        query={graphql`
+  <StaticQuery
+    query={graphql`
       query {
         site {
-          siteMetadata{
+          siteMetadata {
             title
           }
         }
       }
     `}
-
-        render={data => (
-            <nav className={styles.navContainer}>
-                <div className={styles.row}>
-                    <NavLink to='/' text='Articles' />
-                    <NavLink to='/about' text='About' />
-                    <NavLink to='/tags' text='Tags' />
-                </div>
-            </nav>
-
-        )}
-    />
-
-
-
+    render={data => (
+      <nav className={styles.navContainer}>
+        <div className={styles.row}>
+          <NavLink href="/" text="Articles" />
+          <NavLink href="/about" text="About" />
+          <NavLink href="/tags" text="Tags" />
+        </div>
+      </nav>
+    )}
+  />
 )
